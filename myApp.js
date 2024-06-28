c5da9c7ee92cbc3645ee176388dc782322001b88
Podut <podutz1234@gmail.com>
2024-06-28 22:05:43 +0300

    Adaugă funcționalitatea Hello World în myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Definim ruta de bază '/' care va răspunde cu "Hello World"
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  console.log('Hello World'); // Mesajul va fi afișat în consolă
+  res.send('Hello World'); // Înapoiază "Hello World" către client (browser)
 });
 
+// Pornim serverul pe portul 3000
 app.listen(3000, () => {
   console.log('Serverul este pornit pe portul 3000');
 });
